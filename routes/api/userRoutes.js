9c0b82db7afb923c8124163b3696caacff9adc15
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-04-24 21:12:01 -0400

    added user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
